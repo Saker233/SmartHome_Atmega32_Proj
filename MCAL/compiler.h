b15feb66532dec449322bf21f99901bd797df455
Saker233 <130178079+Saker233@users.noreply.github.com>
2023-08-07 00:52:25 +0300

    MCAL Drivers
    Containing all the driver for MCAL which I used in the project

---
+++ MCAL/compiler.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   compiler.h
+ * Author: Mohamed Saker
+ * https://www.linkedin.com/in/mohamed-saker-795123228/
+ */
+
+#ifndef COMPILER_H
+#define	COMPILER_H
+
+#include <xc.h>
+
+#endif	/* COMPILER_H */
+
---
+++ MCAL/std_libraries.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   std_libraries.h
+ * Author: Mohamed Saker
+ * https://www.linkedin.com/in/mohamed-saker-795123228/
+ */
+
+#ifndef STD_LIBRARIES_H
+#define	STD_LIBRARIES_H
+
+/* Section : Includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Section: Macro Declarations */
+
+/* Section: Macro Functions Declarations */
+
+/* Section: Data Type Declarations */
+
+/* Section: Function Declarations */
+
+
+#endif	/* STD_LIBRARIES_H */
+
